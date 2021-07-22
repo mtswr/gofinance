5d5ef118c87d7b53a7cb6b7b37a6e23092289dee
Matheus <mths@edu.unifor.br>
2021-07-22 13:58:32 -0300

    implementação do modal, melhorias e utilização do react hook form

--- src/screens/CategorySelect/index.tsx
+++ src/screens/CategorySelect/index.tsx
@@ -11,7 +11,7 @@ interface Category {
 }
 
 interface Props {
-    category: string;
+    category: Category;
     setCategory: (category: Category) => void;
     closeSelectCategory: () => void;
 }
@@ -20,7 +20,12 @@ export function CategorySelect({
     category,
     setCategory,
     closeSelectCategory
-} : Props) {
+}: Props) {
+    
+    function handleCategorySelect(category: Category) {
+        setCategory(category);
+    }
+
     return (
         <Container>
             <Header>
@@ -32,7 +37,10 @@ export function CategorySelect({
                 style={{ flex: 1, width: '100%' }}
                 keyExtractor={(item) => item.key}
                 renderItem={({ item }) => (
-                    <Category>
+                    <Category
+                        onPress={() => handleCategorySelect(item)}
+                        isActive={category.key === item.key}
+                    >
                         <Icon name={item.icon} />
                         <Name>{item.name}</Name>
                     </Category>
@@ -41,7 +49,10 @@ export function CategorySelect({
             />
     
             <Footer>
-                <Button title="Selecionar"/>
+                <Button
+                    title="Selecionar"                                                                              
+                    onPress={closeSelectCategory}
+                />
             </Footer>
 
 
--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -5,6 +5,8 @@ import { CategorySelectButton } from '../../components/Form/CategorySelectButton
 import { Input } from '../../components/Form/Input';
 import { TransactionTypeButton } from '../../components/Form/TransactionTypeButton';
 import { CategorySelect } from '../CategorySelect';
+import { InputForm } from '../../components/Form/InputForm';
+import { useForm } from 'react-hook-form'
 import {
     Container,
     Header,
@@ -14,11 +16,42 @@ import {
     TransactionsTypes,
 } from './styles';
 
+interface FormData {
+    name: string;
+    amount: string;
+}
+
 export function Register() {
+    const [category, setCategory] = useState({
+        key: 'category',
+        name: 'Categoria',
+    });
     const [transactionType, setTransactionType] = useState('');
+    const [categoryModalOpen, setCategoryModalOpen] = useState(false);
+
+    const { control, handleSubmit} = useForm();
     
     function handleTransactionsTypeSelect(type: 'up' | 'down') {
         setTransactionType(type);
+    }
+
+    function handleCloseSelectCategoryModal() {
+        setCategoryModalOpen(false);
+    }
+
+    function handleOpenSelectCategoryModal() {
+        setCategoryModalOpen(true);
+    }
+
+    function handleRegister(form: FormData) {
+        const data = {
+            name: form.name,
+            amount: form.amount,
+            transactionType,
+            category: category.key,
+        };
+
+        console.log(data);
     }
 
     return (
@@ -28,10 +61,14 @@ export function Register() {
             </Header>
             <Form>
                 <Fields>
-                    <Input
+                    <InputForm
+                        name="name"
+                        control={control}
                         placeholder="Nome"
                     />
-                    <Input
+                    <InputForm
+                        name="amount"
+                        control={control}
                         placeholder="Preço"
                     />
                     <TransactionsTypes>
@@ -49,13 +86,20 @@ export function Register() {
                         />
                     </TransactionsTypes>
 
-                    <CategorySelectButton title="Categoria"/>
+                    <CategorySelectButton
+                        title={category.name}
+                        onPress={handleOpenSelectCategoryModal}
+                    />
                 </Fields>
-                <Button title="Enviar" />
+                <Button title="Enviar" onPress={handleSubmit(handleRegister)} />
             </Form>
 
-            <Modal>
-                <CategorySelect />
+            <Modal visible={categoryModalOpen}>
+                <CategorySelect
+                    category={category}
+                    setCategory={setCategory}
+                    closeSelectCategory={handleCloseSelectCategoryModal}
+                />
             </Modal>
         </Container>
     );
